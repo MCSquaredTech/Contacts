93ad6a58057ab1a8701097d216e0bb9cfd93d535
Mark Christensen <mark@mcsquared.tech>
2023-08-11 08:37:29 -0600

    Third Commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as faIcons from 'react-icons/fa'; 
+import * as bsIcons from 'react-icons/bs';
+import * as riIcons from 'react-icons/ri';
+import * as aiIcons from 'react-icons/ai';
 import { 
-  ProSidebar, 
+  Sidebar, 
   Menu, 
   MenuItem, 
-  SubMenu, 
-  SidebarHeader, 
-  SidebarFooter, 
-  SidebarContent
+  SubMenu
 } from 'react-pro-sidebar';
 
 // https://codesandbox.io/s/react-sidebar-routing-boj4c?file=/src/components/Sidebar.js
 
 import '../pages/Layout.css'
 
-const Sidebar = () => {
+const LeftSidebar = () => {
   const [ collapsed, setCollapsed ] = useState(false); 
   const [ toggled, setToggled ] = useState(false);
 
@@ -28,22 +28,35 @@ const Sidebar = () => {
   }
 
   return (
-    <ProSidebar
+    <Sidebar 
+      className='sidebar'
       collapsed={collapsed}
       toggled={toggled}
       onToggle={handleToggled}
-      breakPoint="md" >
-      {/* ProSidebar Header */}
-      <SidebarHeader>
-        <Menu iconShape="circle">
-          <MenuItem
-            icon={<faIcons.FaAddressBook />}
-            onClick={handleCollapsed}
-            >Contact</MenuItem>
-        </Menu>
-      </SidebarHeader>
-    </ProSidebar>
+      breakPoint="md" >    
+      <Menu>
+        {/* ProSidebar Header */}
+        <h5 style={{textAlign: "center"}}><bsIcons.BsPeopleFill /> Contact List</h5>
+        <MenuItem
+          icon={<aiIcons.AiFillDashboard size={"20px"}/>}
+          component={<Link to="/"  />}
+          >Dashboard</MenuItem>
+        <MenuItem
+          icon={<bsIcons.BsBuildingFill size={"20px"}/>}
+          component={<Link to="/"  />}
+          >Company</MenuItem>
+        <MenuItem
+          icon={<riIcons.RiContactsFill size={"20px"}/>}
+          component={<Link to="/contacts" />} 
+          >Contacts</MenuItem>
+        <MenuItem
+          icon={<faIcons.FaRegIdBadge size={"20px"}/>}
+          component={<Link to="/people" />} 
+          >Card View</MenuItem>
+      </Menu>
+      
+    </Sidebar>
   )
 }
 
-export default Sidebar;
+export default LeftSidebar;
